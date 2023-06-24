e06662e3cab079cd662ebd5a2dce3362e799dfb9
Ashique <ashique12009@gmail.com>
2023-06-24 09:22:27 +0600

    fa added

--- admin/admin-header.php
+++ admin/admin-header.php
@@ -35,6 +35,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../assets/css/custom.css">
     <title><?php echo $page_title != '' ? $page_title : '';?></title>
 </head>
--- admin/admin-home.php
+++ admin/admin-home.php
@@ -7,22 +7,101 @@ require_once 'admin-header.php';
     </div>
 
     <div class="container">
-        <div class="cards-grid-container">
-            <div class="card" style="width: 18rem;">
-                <img src="../assets/images/nopic.png" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text card-description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-3">
+                                <i class="fa fa-video-camera fa-5x"></i>
+                            </div>
+                            <div class="col-9 text-right">
+                                <div class="huge">26k</div>
+                                <div>Total Videos!</div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Details</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
                 </div>
             </div>
-            <div class="card" style="width: 18rem;">
-                <img src="../assets/images/nopic.png" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text card-description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div class="col-lg-3 col-md-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-3">
+                                <i class="fa fa-user fa-5x fa-5x"></i>
+                            </div>
+                            <div class="col-9 text-right">
+                                <div class="huge">100k</div>
+                                <div>Total Users!</div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Details</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
                 </div>
             </div>
-        </div>        
+            <div class="col-lg-3 col-md-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-3">
+                                <i class="fa fa-file-video-o fa-5x fa-5x"></i>
+                            </div>
+                            <div class="col-9 text-right">
+                                <div class="huge">1k</div>
+                                <div>Approved Videos!</div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Details</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-3">
+                                <i class="fa fa-money fa-5x fa-5x"></i>
+                            </div>
+                            <div class="col-9 text-right">
+                                <div class="huge">10k</div>
+                                <div>Total Income!</div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Details</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 
 </body>
